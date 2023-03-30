01e17491241b2cfa74749ee1b93e91f681df22fc
Mirian Santana <miriansantana93@gmail.com>
2023-03-29 22:10:24 -0300

    Added class Request with protocolDelegate and closure

---
+++ ProjectDigimon/ProjectDigimon/ListDigimonViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ListDigimonViewController.swift
+//  ProjectDigimon
+//
+//  Created by Mirian Santana on 29/03/23.
+//
+
+import UIKit
+
+class ListDigimonViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ ProjectDigimon/ProjectDigimon/Request.swift
@@ -0,0 +1,29 @@
+//
+//  Request.swift
+//  ProjectDigimon
+//
+//  Created by Mirian Santana on 29/03/23.
+//
+
+import UIKit
+import Alamofire
+
+protocol RequestDelegate: NSObject {
+    func finishDelegate(arrayDeDigimon: Digimon?)
+}
+
+class Request: NSObject {
+    var arrayDigimon: Digimon?
+    weak var delegate: RequestDelegate?
+    
+    func requestDigimon(completion: @escaping (Digimon?) -> Void) {
+        AF.request("https://digimon-api.vercel.app/api/digimon").response { response in
+            
+            let digimon = try? JSONDecoder().decode(Digimon.self, from: response.data ?? Data())
+            
+            self.arrayDigimon = digimon
+            completion(digimon)
+            self.delegate?.finishDelegate(arrayDeDigimon: digimon)
+        }
+    }
+}
